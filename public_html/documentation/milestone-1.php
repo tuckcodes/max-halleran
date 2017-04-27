3081659eda5a99baeb700de14cf69548906255ad
Emily Halleran <emily@blueroserevival.com>
2017-04-27 07:17:42 -0600

    Test 2

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,16 +9,20 @@
 	<body>
 		<h1>PWP: Online Portfolio for Max Halleran</h1>
 		<h2>Purpose, Audience, Goal</h2>
-			<h3>Purpose</h3>
-				<p>To display my brother Max Halleran's skills as a programmer as well as examples of his programming work on the internet in order to attract new clients and opportunities. </p>
-			<h3>Audience</h3>
-				<p>Visitors to this site will be primarily composed of fellow developers potentially looking to collaborate, potential clients as well as other freelancers who might have a separate skill to collaborate with such as graphic design or content writing.</p>
-			<h3>Goal</h3>
-				<p>To attract future clients and demonstrate offered skills in an attractive manner in order to obtain full, part-time or freelance work.</p>
+		<h3>Purpose</h3>
+		<p>To display my brother Max Halleran's skills as a programmer as well as examples of his programming work on the
+			internet in order to attract new clients and opportunities. </p>
+		<h3>Audience</h3>
+		<p>Visitors to this site will be primarily composed of fellow developers potentially looking to collaborate,
+			potential clients as well as other freelancers who might have a separate skill to collaborate with such as
+			graphic design or content writing.</p>
+		<h3>Goal</h3>
+		<p>To attract future clients and demonstrate offered skills in an attractive manner in order to obtain full,
+			part-time or freelance work.</p>
 
 		<h2>Persona</h2>
 		<table>
-			<thead>Stats</thead>
+			<thead>STATS</thead>
 			<tr>
 				<td>Name:</td>
 				<td>Stewart Donaldson</td>
@@ -31,8 +35,8 @@
 				<td>Job:</td>
 				<td>Recruiter for Software Company</td>
 			</tr>
-				<td>Technology Used:</td>
-				<td>Macbook Pro</td>
+			<td>Technology Used:</td>
+			<td>Macbook Pro</td>
 			<tr>
 				<td>Attitude:</td>
 				<td>Skeptical of new talent, good eye for professionalism</td>
@@ -43,18 +47,41 @@
 			</tr>
 			<tr>
 				<td>Visit Purpose:</td>
-				<td>To Peruse through portfolio and gage skillset/experience for available position in order to know whether or not to contact Max for an interview</td>
+				<td>To Peruse through portfolio and gage skillset/experience for available position in order to know whether
+					or not to contact Max for an interview
+				</td>
 			</tr>
 			<tr>
 				<td>Frustrations:</td>
-				<td>Tired of seeing online portfolios too saturated and difficult to navigate, looking for tactful simplicity.</td>
+				<td>Tired of seeing online portfolios too saturated and difficult to navigate, looking for tactful
+					simplicity.
+				</td>
 			</tr>
-			<tr>Additional Info:</tr>
-			<tr>Stewart will need references upon request and copies of any certifications if he decides on interviewing Max.</tr>
+			<tr>
+				<td>Additional Info:</td>
+				<td>Stewart will need references upon request and copies of any certifications if he decides on interviewing
+					Max.
+				</td>
+			</tr>
 		</table>
-		<p>Stewart Donaldson is a forty-seven year old recruiter for a software company. He likes his Macbook Pro and is skeptical of new talent but has a good eye for spotting true professionalism in candidates. He is currently seeking a junior developer to join the team and came across a link to Max's online portfolio through LinkedIn. His purpose in visiting Max's site is to gage his skillset and experience in relation to the available position in order to know whether or not to contact Max for an interview. Stewart is tired of seeing lacklustre portfolios and resumes. He is tired of having to try to navigate through a less than ideal user experience. He is pleased with the look and function of Max's portfolio. Stewart will need references upon request and copies of any certifications if he decides on interviewing Max. </p>
+		<p>Stewart Donaldson is a forty-seven year old recruiter for a software company. He likes his Macbook Pro and is
+			skeptical of new talent but has a good eye for spotting true professionalism in candidates. He is currently
+			seeking a junior developer to join the team and came across a link to Max's online portfolio through LinkedIn.
+			His purpose in visiting Max's site is to gage his skillset and experience in relation to the available position
+			in order to know whether or not to contact Max for an interview. Stewart is tired of seeing lacklustre
+			portfolios and resumes. He is tired of having to try to navigate through a less than ideal user experience. He
+			is pleased with the look and function of Max's portfolio. Stewart will need references upon request and copies
+			of any certifications if he decides on interviewing Max. </p>
 		<h2>Use Case</h2>
-		<p>Stewart has come across Max's portfolio at about 10AM in the morning while he is sitting at his desk at work. He has just finished his first cup of coffee and it has somehow already started to wear off. He is tired of sifting through inefficiently designed online portfolios and slapped together resumes. He is looking for someone young yet skilled who is able to show you everything you need to know without over-saturating. He is using his relatively new Macbook Pro to access the site and already likes the "smart look" that Max's portfolio gives off. It is a site that is refreshing to come across in today's advertisement filled internet. He rarely uses his mobile device to browse sites like Max's but will do it from time to time on his lunch break, which is why a portfolio that is mobile friendly is also important to him and a factor in the decision process for a new hire.</p>
+		<p>Stewart has come across Max's portfolio at about 10AM in the morning while he is sitting at his desk at work.
+			He has just finished his first cup of coffee and it has somehow already started to wear off. He is tired of
+			sifting through inefficiently designed online portfolios and slapped together resumes. He is looking for
+			someone young yet skilled who is able to show you everything you need to know without over-saturating. He is
+			using his relatively new Macbook Pro to access the site and already likes the "smart look" that Max's portfolio
+			gives off. It is a site that is refreshing to come across in today's advertisement filled internet. He rarely
+			uses his mobile device to browse sites like Max's but will do it from time to time on his lunch break, which is
+			why a portfolio that is mobile friendly is also important to him and a factor in the decision process for a new
+			hire.</p>
 
 
 	</body>
