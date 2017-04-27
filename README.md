1075853c98e939ced3958259a05f15e672ec78cb
Emily Halleran <emily@blueroserevival.com>
2017-04-26 22:25:49 -0600

    Test 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # max-halleran
+a personal website portraying a portfolio about Max Halleran, an up and coming programmer out of Canada. 
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone 1</title>
+	</head>
+
+	<body>
+		<h1>PWP: Online Portfolio for Max Halleran</h1>
+		<h2>Purpose, Audience, Goal</h2>
+			<h3>Purpose</h3>
+				<p>To display my brother Max Halleran's skills as a programmer as well as examples of his programming work on the internet in order to attract new clients and opportunities. </p>
+			<h3>Audience</h3>
+				<p>Visitors to this site will be primarily composed of fellow developers potentially looking to collaborate, potential clients as well as other freelancers who might have a separate skill to collaborate with such as graphic design or content writing.</p>
+			<h3>Goal</h3>
+				<p>To attract future clients and demonstrate offered skills in an attractive manner in order to obtain full, part-time or freelance work.</p>
+
+		<h2>Persona</h2>
+		<table>
+			<thead>Stats</thead>
+			<tr>
+				<td>Name:</td>
+				<td>Stewart Donaldson</td>
+			</tr>
+			<tr>
+				<td>Age:</td>
+				<td>47</td>
+			</tr>
+			<tr>
+				<td>Job:</td>
+				<td>Recruiter for Software Company</td>
+			</tr>
+				<td>Technology Used:</td>
+				<td>Macbook Pro</td>
+			<tr>
+				<td>Attitude:</td>
+				<td>Skeptical of new talent, good eye for professionalism</td>
+			</tr>
+			<tr>
+				<td>Situation:</td>
+				<td>Looking for a junior developer to join the team</td>
+			</tr>
+			<tr>
+				<td>Visit Purpose:</td>
+				<td>To Peruse through portfolio and gage skillset/experience for available position in order to know whether or not to contact Max for an interview</td>
+			</tr>
+			<tr>
+				<td>Frustrations:</td>
+				<td>Tired of seeing online portfolios too saturated and difficult to navigate, looking for tactful simplicity.</td>
+			</tr>
+			<tr>Additional Info:</tr>
+			<tr>Stewart will need references upon request and copies of any certifications if he decides on interviewing Max.</tr>
+		</table>
+
+		<h2>Use Case</h2>
+		<p>Stewart has come across Max's portfolio at about 10AM in the morning while he is sitting at his desk at work. He has just finished his first cup of coffee and it has somehow already started to wear off. He is tired of sifting through inefficiently designed online portfolios and slapped together resumes. He is looking for someone young yet skilled who is able to show you everything you need to know without over-saturating. He is using his relatively new Macbook Pro to access the site and already likes the "smart look" that Max's portfolio gives off. It is refreshing to come across in today's advertisement filled internet.</p>
+
+
+	</body>
+
+</html>
