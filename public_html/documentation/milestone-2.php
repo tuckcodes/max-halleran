02cd24cfa862f2940bfd22051e1d03ea4584b969
Emily Halleran <emily@blueroserevival.com>
2017-05-13 14:00:18 -0600

    Added Wireframes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -15,11 +15,15 @@
 		<h2>Contact Me</h2>
 		<p>This will be a contact form to submit</p>
 		<h2>Graphic Content</h2>
-		<p>Logo,Profile Pic, Animation Photo and Background Photo as well as Speech Bubble.</p>
+		<p>Logo, Profile Pic, Animation Photo and Background Photo as well as Speech Bubble.</p>
 		<h3>Purpose of Content</h3>
 		<p>The purpose of this content is to give the visitor a clear respresentation of the focus and skills that Max has to offer as a developer.</p>
 		<h2>Wireframes</h2>
-		<img src ="images/Wirefram1PWP.png" alt="Wireframe 1"/>
-		<img src ="images/Wireframe2PWP.png" alt="Wireframe2"/>
+		<img src="images/Wirefram1PWP.png" alt="Wireframe 1"/>
+		<img src="images/Wireframe2PWP.png" alt="Wireframe2"/>
+		<img src="images/Wireframe3PWP.png" alt="Wireframe3"/>
+		<img src="images/Wireframe4PWP.png" alt="Wireframe4"/>
+		<img src="images/Wireframe5PWP.png" alt="Wireframe4"/>
+		<img src="images/Wireframe6PWP.png" alt="Wireframe5"/>
 	</body>
 </html>
