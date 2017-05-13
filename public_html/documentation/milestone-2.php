e5d04d434aa98f4d9f75bbe915a0c6e0e7210109
Emily Halleran <emily@blueroserevival.com>
2017-05-13 13:43:06 -0600

    Added written content

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -14,8 +14,10 @@
 		<p>PHP, JAVASCRIPT, CSS HTML</p>
 		<h2>Contact Me</h2>
 		<p>This will be a contact form to submit</p>
+		<h2>Graphic Content</h2>
+		<p>Logo,Profile Pic, Animation Photo and Background Photo as well as Speech Bubble.</p>
 		<h3>Purpose of Content</h3>
-		<p>The purpose of this comtent is to give the visitor a clear respresentation of the focus and skills that Max has to offer as a developer.</p>
+		<p>The purpose of this content is to give the visitor a clear respresentation of the focus and skills that Max has to offer as a developer.</p>
 		<h2>Wireframes</h2>
 		<img src ="images/Wirefram1PWP.png" alt="Wireframe 1"/>
 		<img src ="images/Wireframe2PWP.png" alt="Wireframe2"/>
