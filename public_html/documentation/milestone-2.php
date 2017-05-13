86de849ff8446940f41b55c0e519cc08c3e73690
Emily Halleran <emily@blueroserevival.com>
2017-05-13 13:22:11 -0600

    Milestone 2 First Update

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset = "utf-8"/>
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<h2>Intro</h2>
+		<p>Hey! I'm Max!</p>
+	<h2>Description</h2>
+		<p>I specialize in responsive web development, Wordpress, and Swiss style when it comes to design </p>
+		<h2>Skills</h2>
+		<p>PHP, JAVASCRIPT, CSS HTML</p>
+		<h2>Contact Me</h2>
+		<p>This will be a contact form to submit</p>
+		<h3>Purpose of Content</h3>
+		<p>The purpose of this comtent is to give the visitor a clear respresentation of the focus and skills that Max has to offer as a developer.</p>
+		<h2>Wireframes</h2>
+		<a href="public_html/images/Cruzada (11).png">TEST</a>
+	</body>
+</html>
