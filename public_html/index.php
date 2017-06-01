f47e57474025990eccdad75ff0daa9d1b3ca73a7
Emily Halleran <emily@blueroserevival.com>
2017-06-01 13:53:23 -0600

    Added Recaptcha

--- public_html/index.php
+++ public_html/index.php
@@ -125,7 +125,7 @@
 						</div>
 
 						<!-- reCAPTCHA -->
-						<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+						<div class="g-recaptcha" data-sitekey="6LfuxCMUAAAAAMFnHLmm3rUtZHWvclK0Kka336bq"></div>
 
 						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
 						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
